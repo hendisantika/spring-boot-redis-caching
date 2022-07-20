7441f7f5460f350e211266d62bfa4b1b359b7fd6
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-20 19:47:29 +0700

    #47 - Add getAll in CategoryController :pencil2: :green_book:

--- src/main/java/com/hendisantika/controller/CategoryController.java
+++ src/main/java/com/hendisantika/controller/CategoryController.java
@@ -2,16 +2,19 @@ package com.hendisantika.controller;
 
 import com.hendisantika.dto.CreateCategoryDto;
 import com.hendisantika.model.Category;
+import com.hendisantika.response.CategoryListResponse;
 import com.hendisantika.response.CategoryResponse;
 import com.hendisantika.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,4 +39,10 @@ public class CategoryController {
         return ResponseEntity.ok(new CategoryResponse(category));
     }
 
+    @GetMapping
+    public ResponseEntity<CategoryListResponse> getAll() {
+        List<Category> categories = categoryService.findAll();
+
+        return ResponseEntity.ok(new CategoryListResponse(categories));
+    }
 }
