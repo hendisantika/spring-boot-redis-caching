a718105236d184ee008de88ebbdd5796ffc5bd94
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-20 19:38:17 +0700

    #27 - Add redisConnectionFactory in RedisConfiguration :pencil2: :green_book:

--- src/main/java/com/hendisantika/config/RedisConfiguration.java
+++ src/main/java/com/hendisantika/config/RedisConfiguration.java
@@ -1,6 +1,10 @@
 package com.hendisantika.config;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
@@ -16,4 +20,14 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @Configuration
 @EnableRedisRepositories(value = "com.hendisantika.repository")
 public class RedisConfiguration {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisProperties properties = redisProperties();
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+
+        configuration.setHostName(properties.getHost());
+        configuration.setPort(properties.getPort());
+
+        return new LettuceConnectionFactory(configuration);
+    }
 }
