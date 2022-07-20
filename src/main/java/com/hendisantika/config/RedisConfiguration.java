e0de42bc8b62a6a1590c3b99b024771f202e944e
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-20 19:38:34 +0700

    #28 - Add redisTemplate in RedisConfiguration :pencil2: :green_book:

--- src/main/java/com/hendisantika/config/RedisConfiguration.java
+++ src/main/java/com/hendisantika/config/RedisConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
@@ -29,5 +30,14 @@ public class RedisConfiguration {
         configuration.setPort(properties.getPort());
 
         return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<byte[], byte[]> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(redisConnectionFactory());
+
+        return template;
     }
 }
