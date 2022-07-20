fabd8243c43929e007078ce781d7be4b8bd785d7
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-20 19:42:42 +0700

    #38 - Add search in ProductService :muscle: :whale:

--- src/main/java/com/hendisantika/service/ProductService.java
+++ src/main/java/com/hendisantika/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.hendisantika.service;
 
 import com.hendisantika.dto.CreateProductDto;
+import com.hendisantika.dto.SearchProductDto;
 import com.hendisantika.model.Category;
 import com.hendisantika.model.Product;
 import com.hendisantika.repository.CategoryRepository;
@@ -10,7 +11,17 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.Metamodel;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -55,4 +66,70 @@ public class ProductService {
         return productRepository.findByName(name);
     }
 
+
+    public List<Product> search(SearchProductDto searchProductDto) throws InterruptedException {
+        // Simulate server pressure and high computation
+        Thread.sleep(3000);
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> product = criteriaQuery.from(Product.class);
+
+        Metamodel metamodel = entityManager.getMetamodel();
+        EntityType<Product> productMetaModel = metamodel.entity(Product.class);
+
+        Join<Product, Category> joinCategory = product.join(productMetaModel.getSingularAttribute("category", Category.class));
+
+        List<Predicate> predicateList = new ArrayList<>();
+
+        if (searchProductDto.getName() != null) {
+            predicateList.add(
+                    criteriaBuilder.like(product.get("name"), "%" + searchProductDto.getName().trim() + "%")
+            );
+        }
+
+        if (searchProductDto.getCategory() != null) {
+            predicateList.add(
+                    criteriaBuilder.equal(joinCategory.get("name"), searchProductDto.getCategory())
+            );
+        }
+
+        if (searchProductDto.getMinPrice() > 0 && searchProductDto.getMaxPrice() > 0) {
+            predicateList.add(
+                    criteriaBuilder.between(product.get("price"), searchProductDto.getMinPrice(), searchProductDto.getMaxPrice())
+            );
+        } else {
+            if (searchProductDto.getMinPrice() > 0) {
+                predicateList.add(
+                        criteriaBuilder.gt(product.get("price"), searchProductDto.getMinPrice())
+                );
+            }
+
+            if (searchProductDto.getMaxPrice() > 0) {
+                predicateList.add(
+                        criteriaBuilder.lt(product.get("price"), searchProductDto.getMaxPrice())
+                );
+            }
+        }
+
+        if (searchProductDto.getAvailable() != null) {
+            predicateList.add(criteriaBuilder.equal(product.get("isAvailable"), Objects.equals(searchProductDto.getAvailable(), "yes") ? 1 : 0));
+        }
+
+        Predicate[] predicates = new Predicate[predicateList.size()];
+
+        predicateList.toArray(predicates);
+
+        if (!predicateList.isEmpty()) {
+            criteriaQuery.where(predicates);
+        }
+
+        criteriaQuery.select(product).orderBy(criteriaBuilder.desc(product.get("createdAt")));
+        // criteriaQuery.orderBy(criteriaBuilder.desc(product.get("createdAt")), criteriaBuilder.asc(product.get("price")));
+
+        TypedQuery<Product> query = entityManager.createQuery(criteriaQuery);
+
+        return query.getResultList();
+    }
 }
