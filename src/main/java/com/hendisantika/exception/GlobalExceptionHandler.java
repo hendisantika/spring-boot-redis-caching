6eb147936b731e3b307c0036062889950c8fb195
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-20 19:35:08 +0700

    #19 - Add GlobalExceptionHandler :pencil2: :green_book:

---
+++ src/main/java/com/hendisantika/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.hendisantika.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-redis-caching
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/20/22
+ * Time: 19:34
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
