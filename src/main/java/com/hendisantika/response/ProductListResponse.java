5ceba255b6bc1a286cc603a2101e8584f9af0494
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-20 19:32:38 +0700

    #14 - Add ProductListResponse :pencil2: :green_book:

---
+++ src/main/java/com/hendisantika/response/ProductListResponse.java
@@ -0,0 +1,32 @@
+package com.hendisantika.response;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hendisantika.model.Product;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-redis-caching
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/20/22
+ * Time: 19:32
+ * To change this template use File | Settings | File Templates.
+ */
+@Setter
+@Getter
+@Accessors(chain = true)
+public class ProductListResponse {
+    private List<Product> data;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public ProductListResponse(@JsonProperty("data") List<Product> data) {
+        this.data = data;
+    }
+}
