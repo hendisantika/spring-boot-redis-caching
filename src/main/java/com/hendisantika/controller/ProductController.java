a9cc0b9a144ae0a015b102fd7c46405f2707db1e
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-20 19:48:43 +0700

    #50 - Add getAll in ProductController :pencil2: :green_book:

--- src/main/java/com/hendisantika/controller/ProductController.java
+++ src/main/java/com/hendisantika/controller/ProductController.java
@@ -1,19 +1,26 @@
 package com.hendisantika.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hendisantika.dto.CreateProductDto;
+import com.hendisantika.model.CacheData;
 import com.hendisantika.model.Product;
 import com.hendisantika.repository.CacheDataRepository;
+import com.hendisantika.response.ProductListResponse;
 import com.hendisantika.response.ProductResponse;
 import com.hendisantika.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -40,5 +47,30 @@ public class ProductController {
         Product product = productService.create(createProductDto);
 
         return ResponseEntity.ok(new ProductResponse(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<ProductListResponse> getAll() throws InterruptedException, JsonProcessingException {
+        Optional<CacheData> optionalCacheData = cacheDataRepository.findById("allProducts");
+
+        // Cache hit
+        if (optionalCacheData.isPresent()) {
+            String productAsString = optionalCacheData.get().getValue();
+
+            TypeReference<List<Product>> mapType = new TypeReference<List<Product>>() {
+            };
+            List<Product> productList = objectMapper.readValue(productAsString, mapType);
+
+            return ResponseEntity.ok(new ProductListResponse(productList));
+        }
+
+        // Cache miss
+        List<Product> productList = productService.findAll();
+        String productsAsJsonString = objectMapper.writeValueAsString(productList);
+        CacheData cacheData = new CacheData("allProducts", productsAsJsonString);
+
+        cacheDataRepository.save(cacheData);
+
+        return ResponseEntity.ok(new ProductListResponse(productList));
     }
 }
