4c8355acaab781abd0fc37653ec16430a1392043
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-20 19:39:35 +0700

    #30 - Add CategoryService :muscle: :whale:

---
+++ src/main/java/com/hendisantika/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-redis-caching
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/20/22
+ * Time: 19:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+}
